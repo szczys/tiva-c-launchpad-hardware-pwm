05f57fb1fb1d0dddf07babcfde1696746be91199
Mike Szczys <szczys@hotmail.com>
2014-11-02 18:15:07 -0600

    Created a crude wrapper for Servo objects

--- main.c
+++ main.c
@@ -32,65 +32,136 @@ void delayMS(int ms) {
     SysCtlDelay( (SysCtlClockGet()/(3*1000))*ms ) ;
 }
 
-int
-main(void)
-{
-    //Set the clock
-   SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC |   SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+struct Servo {                      //Examples:
+    uint32_t gpioPeripheral;        //SYSCTL_PERIPH_GPIOF
+    uint32_t pwmModulePeripheral;   //SYSCTL_PERIPH_PWM1
+    uint32_t pinConfig;             //GPIO_PF1_M1PWM5
+    uint32_t portBase;              //GPIO_PORTF_BASE
+    uint8_t gpioPin;                //GPIO_PIN_1
+    uint32_t pwmModuleBase;         //PWM1_BASE
+    uint32_t pwmGenerator;          //PWM_GEN_1 (notice "M1" in pinConfig)
+    uint32_t pwmMode;               //PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC
+    uint32_t period;                //period (SystemClock/PWM_Divider/#_Periods_per_second)
+    uint32_t duty;                  //duty cycle (Period / (100/percent_pin_high_time))
+    uint32_t pwmOut;                //PWM_OUT_5
+    uint32_t pwmOutBit;             //PWM_OUT_5_BIT (notice 'PWM5' in pinConfig)
+};
 
-   //Configure PWM Clock to match system
-   SysCtlPWMClockSet(SYSCTL_PWMDIV_1);
+void pwmClockSetup(uint32_t pwmClockDivider) {
+    SysCtlPWMClockSet(pwmClockDivider);
+}
 
-   // Enable the peripherals used by this program.
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);  //The Tiva Launchpad has two modules (0 and 1). Module 1 covers the LED pins
+struct Servo setupServo(uint32_t gpioPeripheral, uint32_t pwmModulePeripheral, uint32_t pinConfig,
+                    uint32_t portBase, uint8_t gpioPin, uint32_t pwmModuleBase, uint32_t pwmGenerator,
+                    uint32_t pwmMode, uint32_t period, uint32_t duty, uint32_t pwmOut, uint32_t pwmOutBit) {
+    
+    //setup the new struct
+    struct Servo s = { gpioPeripheral, pwmModulePeripheral, pinConfig, portBase, gpioPin, pwmModuleBase, pwmGenerator, pwmMode, period, duty, pwmOut, pwmOutBit };
 
-    //Configure PF1,PF2,PF3 Pins as PWM
-    GPIOPinConfigure(GPIO_PF1_M1PWM5);
-    GPIOPinConfigure(GPIO_PF2_M1PWM6);
-    GPIOPinConfigure(GPIO_PF3_M1PWM7);
-    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+    // Enable the peripherals used by this program.
+    SysCtlPeripheralEnable(gpioPeripheral);
+    SysCtlPeripheralEnable(pwmModulePeripheral);
+
+    //Configure pins PWM
+    GPIOPinConfigure(pinConfig);
+    //Configure PWM type
+    GPIOPinTypePWM(portBase, gpioPin);
 
     //Configure PWM Options
-    //PWM_GEN_2 Covers M1PWM4 and M1PWM5
-    //PWM_GEN_3 Covers M1PWM6 and M1PWM7 See page 207 4/11/13 DriverLib doc
-    PWMGenConfigure(PWM1_BASE, PWM_GEN_2, PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC); 
-    PWMGenConfigure(PWM1_BASE, PWM_GEN_3, PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC); 
+    PWMGenConfigure(pwmModuleBase, pwmGenerator, pwmMode);
 
     //Set the Period (expressed in clock ticks)
-    PWMGenPeriodSet(PWM1_BASE, PWM_GEN_2, 320);
-    PWMGenPeriodSet(PWM1_BASE, PWM_GEN_3, 320);
+    PWMGenPeriodSet(pwmModuleBase, pwmGenerator, period);
 
-    //Set PWM duty-50% (Period /2)
-    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_5,100);
-    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6,100);
-    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_7,100);
+    //Set PWM duty
+    PWMPulseWidthSet(pwmModuleBase, pwmOut, duty);
 
     // Enable the PWM generator
-    PWMGenEnable(PWM1_BASE, PWM_GEN_2);
-    PWMGenEnable(PWM1_BASE, PWM_GEN_3);
+    PWMGenEnable(pwmModuleBase, pwmGenerator);
 
     // Turn on the Output pins
-    PWMOutputState(PWM1_BASE, PWM_OUT_5_BIT | PWM_OUT_6_BIT | PWM_OUT_7_BIT, true);
+    PWMOutputState(pwmModuleBase, pwmOutBit, true);
+
+    //return the struct so the calling code has a reference to it
+    return s;
+}
+
+void setDuty(struct Servo s, uint32_t duty) {
+    s.duty = duty;
+    PWMPulseWidthSet(s.pwmModuleBase, s.pwmOut, duty);
+}
+
+int main(void)
+{
+/*
+need to know the pwm divider
+
+each object:
+    -gpio register
+    -module and pwm output configuration declaration
+    -port base
+    -gpio pin
+    -pwmbase
+    -pwm generator
+    -pwm mode
+    -period
+    -duty
+    -pwmoutbit
+*/
+    
+    //Set the clock
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC |   SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+
+    //Configure PWM Clock to divide system by 64
+    pwmClockSetup(SYSCTL_PWMDIV_64);
+
+    uint32_t period = 5000;
+    uint32_t duty = 5000;
+
+    //Setup a servo on PF1
+    struct Servo sPF1 = setupServo(    SYSCTL_PERIPH_GPIOF,
+                                SYSCTL_PERIPH_PWM1,
+                                GPIO_PF1_M1PWM5,
+                                GPIO_PORTF_BASE,
+                                GPIO_PIN_1,
+                                PWM1_BASE,
+                                PWM_GEN_2,
+                                PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC,
+                                period,
+                                duty,
+                                PWM_OUT_5,
+                                PWM_OUT_5_BIT
+                            );
+
+
+    /*
+    // Unlock the Pin PF0 and Set the Commit Bit
+    // See datasheet table 10-1 for explanation of
+    // why this pin needs unlocking
+    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
+    HWREG(GPIO_PORTF_BASE + GPIO_O_CR)   |= 0x01;
+    */
+
 
     //Fade
     bool fadeUp = true;
-    unsigned long increment = 10;
-    unsigned long pwmNow = 100;
+    uint32_t increment = 10;
+
+
     while(1)
-    {
-        delayMS(20);
+    {   
+        delayMS(2);
         if (fadeUp) {
-            pwmNow += increment;
-            if (pwmNow >= 320) { fadeUp = false; }
+            duty += increment;
+            if (duty >= period) { fadeUp = false; }
         }
         else {
-            pwmNow -= increment;
-            if (pwmNow <= 10) { fadeUp = true; }
-        }
-        PWMPulseWidthSet(PWM1_BASE, PWM_OUT_5,pwmNow);
-        PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6,pwmNow);
-        PWMPulseWidthSet(PWM1_BASE, PWM_OUT_7,pwmNow);
+            duty -= increment;
+            if (duty <= increment) { fadeUp = true; }
+        }    
+        
+        setDuty(sPF1,duty);
+
     }
 
 }
